04f548318a885afc614b5d17232272a030d1737e
Haider Mehdi <haidershah659@gmail.com>
2023-10-18 18:24:54 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const names = ["Shareef", "Shafqat", "Shakeel", "Shafeeq", "Shameem"];
+for (const name of names) {
+    console.log(name);
+}
+console.log('-'.repeat(100));
+// Second Way
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ index.ts
@@ -0,0 +1,14 @@
+const names: string[] = ["Shareef", "Shafqat", "Shakeel", "Shafeeq", "Shameem"];
+
+for (const name of names) {
+    console.log(name);
+}
+
+console.log('-' .repeat(100));
+// Second Way
+
+for (let i=0; i< names.length; i++)
+{
+    console.log(names[i]);
+}
+export{};
